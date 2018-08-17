01f4a08edf2b138863eab29749280bd3429f1eec
catarinawor <catarinawor@gmail.com>
2018-08-17 11:27:04 -0700

    add directories example

---
+++ R/directories_example.R
@@ -0,0 +1,19 @@
+#========================================================
+#Directories where model routines, data analysis routines and data files
+#are found. Change these to fit the paths in your computer and save the file as directories.R
+#Author: Catarina Wor
+#February 2017
+#========================================================
+
+#this is the directory for your modeling files 
+model_dir<-"C:/path/to/CYER/R"
+
+#this is the directory for your raw data files are and where you save the processed data
+data_dir<-"C:/path/to/CYER/data/"
+
+#This is where you store your figures.
+figs_dir<-"C:/path/to/CYER/figs/"
+
+#This is where you store your texfiles.
+tex_dir<-"C:/path/to/CYER/tex"
+
